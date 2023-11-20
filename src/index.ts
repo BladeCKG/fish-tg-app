8b3fe69c31219c59031c089cc192085e31774ca5
BladeCKG <neverdreamagain9106@gmail.com>
2023-11-20 12:50:17 -0800

    init

---
+++ src/index.ts
@@ -0,0 +1,101 @@
+import { Api, TelegramClient } from "telegram";
+import { CustomFile } from "telegram/client/uploads";
+import { StoreSession, StringSession } from "telegram/sessions";
+const sentences = require("./sentences.json");
+const input = require("input");
+const fetchChannels = require("./channels.json");
+const users = require("./users.json");
+const app = require("./app.json");
+
+const apiId = app.apiId;
+const apiHash = app.apiHash;
+for (const user of users) {
+  const session = user.session;
+  const stringSession = new StringSession(session); // fill this later with the value from session.save()
+
+  (async () => {
+    console.log("Loading interactive example...");
+    const client = new TelegramClient(stringSession, apiId, apiHash, {
+      connectionRetries: 5,
+    });
+    await client.start({
+      phoneNumber:
+        // (user.phone as string) ||
+        async () => await input.text("Please enter your number: "),
+      password: async () => await input.text("Please enter your password: "),
+      phoneCode: async () =>
+        await input.text("Please enter the code you received: "),
+      onError: (err) => console.log(err),
+    });
+    console.log("You should now be connected.");
+    console.log(client.session.save()); // Save this string to avoid logging in again
+
+    // console.log(await client.getParticipants("@XWINENRCAP", { limit: 100 }));
+    const offset = 0;
+    const limit = 100;
+    await client.invoke(
+      new Api.channels.JoinChannel({ channel: "@toremifasolla" })
+    );
+    let totalInvitedMember = 0;
+    for (const channel of fetchChannels) {
+      const users = await client.getParticipants(channel, {
+        showTotal: true,
+      });
+      totalInvitedMember += users.length;
+      console.log(
+        "Total: %d, fetched: %d, Total invited: %d",
+        users.total,
+        users.length,
+        totalInvitedMember
+      );
+
+      for (const user of users) {
+        try {
+          // console.log(user.username);
+          // await client.invoke(
+          //   new Api.channels.InviteToChannel({
+          //     channel: "@toremifasol",
+          //     users: [user.username as EntityLike],
+          //   })
+          // );
+        } catch (error) {}
+      }
+    }
+    // await client.sendMessage("@ericbain1260", {
+    //   message: "this is just for test. dont reply, bro",
+    // });
+    // await client.invoke(
+    //   new Api.channels.InviteToChannel({
+    //     channel: "@toremifasol",
+    //     users: ["@imoliverlim"],
+    //   })
+    // );
+    //https://t.me/VenomFoundation_AirDrop/204831
+    const func1 = () =>
+      setTimeout(async () => {
+        try {
+          await client.forwardMessages("@toremifasolla", {
+            fromPeer: "@VenomFoundation_AirDrop",
+            messages: 204831,
+          });
+        } catch (error) {}
+        await client.sendFile("@toremifasolla", {
+          file: "./airdrop.webp",
+          caption: "hello",
+        });
+        await client.sendMessage("@toremifasolla", {
+          message:
+            sentences[
+              Math.max(Math.ceil(Math.random() * sentences.length) - 1, 0)
+            ],
+        });
+
+        // await client.sendFile("@toremifasol", {
+        //   file: "https://t.me/toremifasol/231",
+        //   caption: "It's me!",
+        // });
+        func1();
+      }, Math.ceil(Math.random() * 30) * 1000);
+    func1();
+  })();
+}
