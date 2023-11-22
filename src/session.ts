5d2a02485a74e8e888cbe6121510dd12d22537ed
BladeCKG <neverdreamagain9106@gmail.com>
2023-11-22 05:58:33 -0800

    forwarder

--- src/session.ts
+++ src/session.ts
@@ -4,12 +4,12 @@ import { StoreSession, StringSession } from "telegram/sessions";
 import * as fs from "fs";
 const sentences = require("./sentences.json");
 const input = require("input");
-const forwardChannels = require("./forward_channels.json");
+const fetchChannels = require("./channels.json");
 const users = require("./users.json");
 const bot = require("./bot.json");
 const app = require("./app.json");
 const airdropMsg = fs.readFileSync("text.txt", { encoding: "utf8" });
-const chatGroup = "@zksync_airdrop_group_chat";
+const chatGroup = "@toremi1";
 
 const apiId = app.apiId;
 const apiHash = app.apiHash;
@@ -19,7 +19,7 @@ const apiHash = app.apiHash;
     const session = user.session;
     const stringSession = new StringSession(session); // fill this later with the value from session.save()
 
-    (async () => {
+    await (async () => {
       console.log("Connecting user: %s", user.name);
       const client = new TelegramClient(stringSession, apiId, apiHash, {
         connectionRetries: 5,
@@ -35,25 +35,6 @@ const apiHash = app.apiHash;
       });
       console.log("You should now be connected.");
       console.log(client.session.save()); // Save this string to avoid logging in again
-
-      await client.invoke(new Api.channels.JoinChannel({ channel: chatGroup }));
-      const func1 = () =>
-        setTimeout(async () => {
-          const channel =
-            forwardChannels[Math.ceil(Math.random() * forwardChannels.length)];
-          console.log("Forwarding channel to %s from %s", channel, user.name);
-          try {
-            await client.invoke(
-              new Api.channels.JoinChannel({ channel: channel })
-            );
-            await client.forwardMessages(channel, {
-              fromPeer: chatGroup,
-              messages: 6,
-            });
-          } catch (error) {}
-          func1();
-        }, Math.ceil(Math.random() * 60 * 10) * 1000);
-      func1();
     })();
   }
 })();
